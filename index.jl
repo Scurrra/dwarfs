7233310c085e5f08a75fe92a9c6f585c64898527
Scurrra <40835268+Scurrra@users.noreply.github.com>
2025-08-04 18:04:26 +0000

    Deploying to gh-pages from @ Scurrra/dwarfs@9b6b9bed728cac0270a9b9a1ecd0ab720ec4d8b4 🚀

---
+++ index.jl
@@ -0,0 +1,38 @@
+### A Pluto.jl notebook ###
+# v0.20.13
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7f8d7c48-715a-11f0-2f44-5d9fb8b4207f
+md"""
+# Dwarfs are small planets like Pluto
+"""
+
+# ╔═╡ 804b7c16-7ddf-401b-a7ff-6f97d5b7a163
+md"""
+> ## [True One](true_one.html)
+> Agent-based modeling of the game "True American" from tv series "New Girl".
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.11.6"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─7f8d7c48-715a-11f0-2f44-5d9fb8b4207f
+# ╟─804b7c16-7ddf-401b-a7ff-6f97d5b7a163
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
